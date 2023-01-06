0adcafa2cea6c88731a16222c3cbe104d09106cf
jiamtorres <elpiso5b@gmail.com>
2023-01-06 19:21:23 -0400

    Initial commit

---
+++ src/com/opboot/Ejercicio1.java
@@ -0,0 +1,21 @@
+package com.opboot;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+
+        int num1 = 3;
+        long num2 = 900;
+        double num3 = 12.56;
+
+        boolean var1 = false;
+
+        String texto = "Me gusta el jamón";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(var1);
+        System.out.println(texto);
+    }
+}
